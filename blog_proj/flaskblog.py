100fe7dd686aa6d1218dc4946709660f29c40ebc
NASSERHAMAD <nasserrww19@gmail.com>
2023-03-15 20:20:08 +0300

    app run

--- blog_proj/flaskblog.py
+++ blog_proj/flaskblog.py
@@ -1,7 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
-app.secret_key = 'fdb1eefad885bf835e1dea884244221'
+
 
 @app.route("/")
 def hello():
     return "hello world!"
+app.run( debug=True )
