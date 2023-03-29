9045003c46f7f0a69e28fe39e1f0ff3a2623afa0
NASSERHAMAD <nasserrww19@gmail.com>
2023-03-29 22:22:49 +0300

    fixing bugs related with fontawsome

---
+++ templates/mobile-data.js
@@ -0,0 +1,42 @@
+var mobileData = [
+	{
+	  name: "iPhone 13",
+	  brand: "Apple",
+	  price: "$999",
+	  ram: "6GB",
+	  storage: "128GB",
+	  "image": "https://fakeimg.pl/300x200/"
+	},
+	{
+	  name: "Galaxy S21",
+	  brand: "Samsung",
+	  price: "$799",
+	  ram: "8GB",
+	  storage: "128GB",
+	  "image": "https://fakeimg.pl/300x200/"
+	},
+	{
+	  name: "Pixel 6",
+	  brand: "Google",
+	  price: "$699",
+	  ram: "8GB",
+	  storage: "128GB",
+	  "image": "https://fakeimg.pl/300x200/"
+	},
+	{
+	  name: "OnePlus 9",
+	  brand: "OnePlus",
+	  price: "$729",
+	  ram: "8GB",
+	  storage: "128GB",
+	  "image": "https://fakeimg.pl/300x200/"
+	},
+	{
+	  name: "Mi 12",
+	  brand: "Xiaomi",
+	  price: "$699",
+	  ram: "12GB",
+	  storage: "256GB",
+	  "image": "https://fakeimg.pl/300x200/"
+	}
+  ];
