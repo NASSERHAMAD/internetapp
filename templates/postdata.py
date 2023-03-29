193680d1566d6cb5521fb12d7e287c718c2a2379
NASSERHAMAD <nasserrww19@gmail.com>
2023-03-29 17:03:03 +0300

    deleting layout file

---
+++ templates/postdata.py
@@ -0,0 +1,6 @@
+from flask import Flaskapp
+= Flask(__name__)@app.route("/")def hello():return 
+"<h1>Hello World!Sami<h1>"@app.route("/about")def 
+about():return "<h1>About Page<h1>"
+if __name__ == '__main__'
+:app.run( debug=True )
