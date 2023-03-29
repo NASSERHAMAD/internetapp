d44f332e6135340757d37afc10b9a9b3641b14eb
NASSERHAMAD <nasserrww19@gmail.com>
2023-03-29 21:59:16 +0300

    fix readme and adding bootstrab to all pages

--- static/mobile-data.js
+++ static/mobile-data.js
@@ -1,68 +1,39 @@
-var mobiles = [
-    {
-      "id": 1,
-      "brand": "Apple",
-      "model": "iPhone 12","price": 799,
-      "image": "https://fakeimg.pl/300x200/"
-      },
-      {
-      "id": 2,
-      "brand": "Samsung",
-      "model": "Galaxy S21",
-      "price": 699,
-      "image": "https://fakeimg.pl/300x200/"
-      },
-      {
-      "id": 3,
-      "brand": "OnePlus",
-      "model": "8T",
-      "price": 599,
-      "image": "https://fakeimg.pl/300x200/"
-      },
-      {
-      "id": 4,
-      "brand": "Google",
-      "model": "Pixel 5",
-      "price": 699,
-      "image": "https://fakeimg.pl/300x200/"
-      },
-      {
-      "id": 5,
-      "brand": "Xiaomi",
-      "model": "Mi 11",
-      "price": 699,
-      "image": "https://fakeimg.pl/300x200/"
-      }
-      ];
-      
-      var mobilesContainer = document.querySelector("#mobiles-container");
-      
-      for (var i = 0; i < mobiles.length; i++) {
-      var mobile = mobiles[i];
-      
-      var card = document.createElement("div");
-      card.classList.add("card");
-      
-      var image = document.createElement("img");
-      image.classList.add("card-img-top");
-      image.setAttribute("src", mobile.image);
-      
-      var cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-      
-      var title = document.createElement("h5");
-      title.classList.add("card-title");
-      title.textContent = mobile.brand + " " + mobile.model;
-      
-      var price = document.createElement("p");
-      price.classList.add("card-text");
-      price.textContent = "$" + mobile.price;
-      
-      cardBody.appendChild(title);
-      cardBody.appendChild(price);
-      
-      card.appendChild(image);
-      card.appendChild(cardBody);
-      
-      mobilesContainer.appendChild(card);
-      }
+var mobileData = [
+	{
+		name: "iPhone 13",
+		brand: "Apple",
+		price: "$999",
+		ram: "6GB",
+		storage: "128GB"
+	},
+	{
+		name: "Galaxy S21",
+		brand: "Samsung",
+		price: "$799",
+		ram: "8GB",
+		storage: "128GB"
+	},
+	{
+		name: "Pixel 6",
+		brand: "Google",
+		price: "$699",
+		ram: "8GB",
+		storage: "128GB"
+	},
+	{
+		name: "OnePlus 9",
+		brand: "OnePlus",
+		price: "$729",
+		ram: "8GB",
+		storage: "128GB"
+	},
+	{
+		name: "Mi 12",
+		brand: "Xiaomi",
+		price: "$699",
+		ram: "12GB",
+		storage: "256GB"
+	}
+];
+
+console.log("Mobile data loaded successfully!");
