ca10289ac2a53979e1a29128f881c2eab0e69207
NASSERHAMAD <nasserrww19@gmail.com>
2023-04-10 22:46:11 +0300

    12,13,14

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 from flask import Flask, render_template
+from postdata import posts_data
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html')
+if __name__ == '__main__':
+    app.run()
+    
+app = Flask(__name__)
 
-# Run the Flask app
+@app.route('/')
+def posts():
+    return render_template('post_all.html', posts_data=posts_data)
+if __name__ == '__main__':
+    app.run(debug=True)    
+
+app = Flask(__name__)
+
+@app.route('/posts/<int:post_id>')
+def post(post_id):
+    post = None
+    for p in posts_data:
+        if p['id'] == post_id:
+            post = p
+            break
+    return render_template('post-single.html', post=post)
+
 if __name__ == '__main__':
     app.run(debug=True)
--- postdata.py
+++ postdata.py
@@ -1,5 +1,20 @@
-def get_posts():
-    # Fetch post data from data source
-    # Example:
-    posts = [{'title': 'Post 1', 'content': 'Content 1'}, {'title': 'Post 2', 'content': 'Content 2'}]
-    return posts
+posts_data = [
+    {
+        "title": "Post 1",
+        "author": "John Doe",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "date": "2022-01-01"
+    },
+    {
+        "title": "Post 2",
+        "author": "Jane Smith",
+        "content": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem.",
+        "date": "2022-01-02"
+    },
+    {
+        "title": "Post 3",
+        "author": "Bob Johnson",
+        "content": "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium.",
+        "date": "2022-01-03"
+    }
+]
