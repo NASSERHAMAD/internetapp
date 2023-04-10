6bd3fd98eb713cbd96f09ff2ed1e19bb7a7c5a32
NASSERHAMAD <nasserrww19@gmail.com>
2023-04-10 03:09:49 +0300

    phase 4 test

---
+++ postdata.py
@@ -0,0 +1,5 @@
+def get_posts():
+    # Fetch post data from data source
+    # Example:
+    posts = [{'title': 'Post 1', 'content': 'Content 1'}, {'title': 'Post 2', 'content': 'Content 2'}]
+    return posts
