983e68627bc1b6206f8fe290dd0acd88ba3dc943
NASSERHAMAD <nasserrww19@gmail.com>
2023-04-10 21:48:38 +0300

    f

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-@app.route('/mo')
-def home():
-    return render_template('mobiles.html')
+    return render_template('home.html', title='Home')
 
+# Run the Flask app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
